4177ce512d4dd001c99acf1814c41cdcb250154b
Ghufran0987 <ghufranm054@gmail.com>
2024-05-18 03:27:04 +0500

    Add Categories and filter data

--- src/views/IndexView.jsx
+++ src/views/IndexView.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import '../assets/css/style.css';
-import '../assets/css/global.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import NavbarComponent from '../component/Navbar';
 import SearchBar from '../component/SearchBar';
 import Footer from '../component/Footer';
 import Category from '../component/Category';
 import Listings from './Listing';
-function IndexView({ handleClose }) {
 
+function IndexView() {
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategoryId(categoryId);
+  };
 
   return (
     <>
-       <NavbarComponent/>
-       <SearchBar/>
-       <Category/>
-       <Listings/>
-        <Footer/>
-        
+      <NavbarComponent />
+      <SearchBar />
+      <Category onCategorySelect={handleCategorySelect} />
+      <Listings categoryId={selectedCategoryId} />
+      <Footer />
     </>
- 
   );
 }
 
